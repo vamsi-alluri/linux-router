7301b81b20847e1825d5e4adb8fa5a7b5272af5d
Vamsi Alluri <vamsi1306@gmail.com>
2025-04-01 18:26:36 -0400

    pipes and processes on router

---
+++ src/dnsd.h
@@ -0,0 +1,6 @@
+#ifndef DNSD_H
+#define DNSD_H
+
+void dns_main(int rx_fd, int tx_fd);
+
+#endif /* DNSD_H */
---
+++ src/ntpd.h
@@ -0,0 +1,6 @@
+#ifndef NTPD_H
+#define NTPD_H
+
+void ntp_main(int rx_fd, int tx_fd);
+
+#endif /* NTPD_H */
